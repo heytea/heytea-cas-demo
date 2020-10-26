8b6632117088c1176c12a8e8208be60e3d54d6f7
WEIZIBIN <danielweiup@gmail.com>
2020-10-26 17:27:02 +0800

    员工账号dto

---
+++ heytea-demo-rabbitmq-consumer/src/main/java/com/heytea/demo/rabbitmq/model/dto/SfEmployeeJob.java
@@ -0,0 +1,136 @@
+package com.heytea.demo.rabbitmq.model.dto;
+
+import lombok.Data;
+import lombok.experimental.Accessors;
+
+/**
+ * @author weizibin
+ * @since 2020/10/26 下午5:22
+ */
+@Data
+@Accessors(chain = true)
+public class SfEmployeeJob {
+	/**
+	 * 工号
+	 */
+	private String userId;
+
+	/**
+	 * 职位编号
+	 */
+	private String position;
+
+	/**
+	 * 板块
+	 */
+	private String company;
+
+	/**
+	 * 中心
+	 */
+	private String businessUnit;
+
+	/**
+	 * 部门
+	 */
+	private String division;
+
+	/**
+	 * 组别
+	 */
+	private String department;
+
+	/**
+	 * 区域/组别
+	 */
+	private String customString7;
+
+	/**
+	 * 门店
+	 */
+	private String customString8;
+
+	/**
+	 * sf部门id
+	 */
+	private String customString4;
+
+	/**
+	 * 门店
+	 */
+	private String customString14;
+
+	/**
+	 * 事件原因
+	 */
+	private String eventReason;
+
+	/**
+	 * 城市代码
+	 */
+	private String customString3;
+
+	/**
+	 * 员工类型：5831-实习生
+	 */
+	private String employeeType;
+
+	/**
+	 * 最后修改时间
+	 */
+	private String lastModifiedOn;
+
+	/**
+	 * 工作类型：5919-门店；5920-职能
+	 */
+	private String customString21;
+
+	/**
+	 * 职务等级
+	 */
+	private String customString22;
+
+	/**
+	 * 状态
+	 */
+	private String emplStatus;
+
+	/**
+	 * 职位名称
+	 */
+	private String positionName;
+
+	/**
+	 * 员工姓名
+	 */
+	private String personName;
+	/**
+	 * 性别
+	 */
+	private String sex;
+	/**
+	 * 生日
+	 */
+	private String birthday;
+	/**
+	 * 手机号
+	 */
+	private String mobile;
+	/**
+	 * 公司邮箱
+	 */
+	private String companyEmail;
+	/**
+	 * 企业邮箱
+	 */
+	private String personalEmail;
+	/**
+	 * 证件号码
+	 */
+	private String nationalId;
+	/**
+	 * 证件类型
+	 */
+	private String cardType;
+
+}
